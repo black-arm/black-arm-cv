3de5c6d0930610e8edfcfe783bd29d62e3a730f1
Antonio Basile <antoniobasile2@eng.it>
2023-10-05 21:57:12 +0200

    feat: navbar created

---
+++ cypress/e2e/homepage.cy.ts
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+  it('should active dark mode', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-testid="darkModeButton"]').dblclick()
+    cy.get('[data-theme="dark"]').should('exist')
+  })
+})
